e9c18af70766aab3b7f03c616992bd893073d0d0
pisa-kun <pisa0401mst@gmail.com>
2020-05-25 01:32:27 +0900

    first commit

---
+++ UserControlSample/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserControlSample
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string input = "Default Message";
+        public string Input
+        {
+            get => input;
+            set
+            {
+                input = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Input)));
+            }
+        }
+
+    }
+}
